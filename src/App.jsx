76e4ca2a2ac1f7abbb805c277df70692305489f1
EoinCous <eoin.cousins@gmail.com>
2025-04-28 15:35:41 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {return (
+  <>
+    <MovieCard movie={{title: "Eoin's movie", release_date: "2025"}}/>
+  </>
+  )
+}
+
+export default App
